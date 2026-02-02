7d87fe473c439753250d2569eff551072b296843
SanEk <tt.worm524@gmail.com>
2026-02-02 16:13:51 +0200

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace mvc
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Director { get; set; }
+        public string? Genre { get; set; }
+        public int Year { get; set; }
+        public string? Poster { get; set; }
+        public string? Description { get; set; }
+    }
+}
